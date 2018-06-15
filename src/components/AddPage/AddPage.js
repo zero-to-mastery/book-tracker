a37c53ce48a01dbd18b96ddd0b3958beadc089c5
gillenha <gillen08@gmail.com>
2018-06-14 21:42:39 -0400

    added event listeners to the add a book page

--- src/components/AddPage/AddPage.js
+++ src/components/AddPage/AddPage.js
@@ -1,38 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './AddPage.css';
 
-
-const AddPage = () => {    
-
+class AddPage extends Component {    
+    render() {
     return (
-        <div>										
-				
-                <div className="mainContent">
-                    <h1>Add book</h1>
-                    <div>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <td>Title</td>
-                                    <td>Author</td>
-                                    <td>Progress</td>
-                                </tr>
-                            </thead>    
-                            <tbody>
-                                <tr >
-                                    <td><input type="text" name="title" /></td>
-                                    <td><input tyoe="text" name="author" /></td>
-                                    <td><input type="text" name="progress" /></td>
-                                </tr>
-                            </tbody>                
-                        </table>
-                        <input type="radio" name="list" value="wish" checked/>Wish List<br />
-                        <input type="radio" name="list" value="reading" />Reading List<br />
-                        <a className="btn btn-basic btn-sm" href="">Add</a>
-                    </div>
+        <div>                                        
+            <div className="mainContent">
+                <h1>Add book</h1>
+                <div>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <td>Title</td>
+                                <td>Author</td>
+                                <td>Progress</td>
+                            </tr>
+                        </thead>    
+                        <tbody>
+                            <tr >
+                                <td><input onChange={event => console.log(event.target.value)} type="text" name="title" /></td>
+                                <td><input onChange={event => console.log(event.target.value)} type="text" name="author" /></td>
+                                <td><input onChange={event => console.log(event.target.value)} type="text" name="progress" /></td>
+                            </tr>
+                        </tbody>                
+                    </table>
+                    <input type="radio" name="list" value="wish" checked/>Wish List<br />
+                    <input type="radio" name="list" value="reading" />Reading List<br />
+                    <a className="btn btn-basic btn-sm" href="">Add</a>
                 </div>
+            </div>
     </div>
     );
+    }
 };
 
 export default AddPage;
