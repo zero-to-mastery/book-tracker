f0c016de794fec3fb8bbbda0fb89024929827d16
Chris Pfaff <chrispfaff10@gmail.com>
2018-12-08 09:15:03 -0600

    changed href to button to avoid accessibilty error

--- src/components/ReadingList/ReadingList.js
+++ src/components/ReadingList/ReadingList.js
@@ -1,40 +1,46 @@
-import React from 'react';
-import './ReadingList.css';
+import React from "react";
+import "./ReadingList.css";
 
-const ReadingList = ({data}) => {
-    return (
-        <div>     
-        <div className="mainContent">
-            <h1>Progress on current books</h1>
-            <div>
-                
-            <table className="table">
-                <thead>
-                    <tr>
-                        <td>Title</td>
-                        <td>Author</td>
-                        <td>Progress</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </thead>
-                <tbody>
-                {data.map((book,index) => (
-                    <tr className="bookRow" key={book.id}>
-                        <td className="">{book.title}</td>
-                        <td>{book.author}</td>
-                        <td>{book.page}</td>
-                        <td><a key={book.id} className="btn btn-basic btn-sm" href="">Edit</a></td>
-                        <td><a key={book.id} className="btn btn-basic btn-sm" href="">Delete</a></td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            
-            </div>
+const ReadingList = ({ data }) => {
+  return (
+    <div>
+      <div className="mainContent">
+        <h1>Progress on current books</h1>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <td>Title</td>
+                <td>Author</td>
+                <td>Progress</td>
+                <td />
+                <td />
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((book, index) => (
+                <tr className="bookRow" key={book.id}>
+                  <td className="">{book.title}</td>
+                  <td>{book.author}</td>
+                  <td>{book.page}</td>
+                  <td>
+                    <button key={book.id} className="btn btn-basic btn-sm">
+                      Edit
+                    </button>
+                  </td>
+                  <td>
+                    <button key={book.id} className="btn btn-basic btn-sm">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        </div>                       
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ReadingList;
--- src/components/WishList/WishList.js
+++ src/components/WishList/WishList.js
@@ -1,40 +1,46 @@
-import React from 'react';
-import './WishList.css';
+import React from "react";
+import "./WishList.css";
 
-
-const WishList = ({data, props}) => {    
-
-    return (
+const WishList = ({ data, props }) => {
+  return (
+    <div>
+      <div className="mainContent">
+        <h1>Books on My Wish List</h1>
         <div>
-            <div className="mainContent">
-                <h1>Books on My Wish List</h1>
-                <div>
-                    <table className="table" data={this.data}>
-                        <thead>
-                            <tr>
-                                <td>Title</td>
-                                <td>Author</td>
-                                <td>Progress</td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </thead>    
-                        <tbody>
-                        {data.map((book, index) => (
-                            <tr key={book.id}>
-                                <td>{book.title}</td>
-                                <td>{book.author}</td>
-                                <td>{book.page}</td>
-                                <td><a className="btn btn-basic btn-sm" href="">Edit</a></td>
-                                <td><a className="btn btn-basic btn-sm" href="">Delete</a></td>
-                            </tr>
-                        ))}
-                        </tbody>                
-                    </table>
-                </div>
-            </div>
+          <table className="table" data={this.data}>
+            <thead>
+              <tr>
+                <td>Title</td>
+                <td>Author</td>
+                <td>Progress</td>
+                <td />
+                <td />
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((book, index) => (
+                <tr key={book.id}>
+                  <td>{book.title}</td>
+                  <td>{book.author}</td>
+                  <td>{book.page}</td>
+                  <td>
+                    <button className="btn btn-basic btn-sm" href="">
+                      Edit
+                    </button>
+                  </td>
+                  <td>
+                    <button className="btn btn-basic btn-sm" href="">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default WishList;
