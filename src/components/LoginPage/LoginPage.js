b9887ab9c52d890bf1110d0571c7470407dd2722
darklordace <firdaus.alghifari@gmail.com>
2019-01-20 14:33:45 +0700

    LoginPage added

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -25,46 +25,44 @@ class LoginPage extends Component {
 
   render() {
   	return (
-  		<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-				<main className="pa4 black-80">
-		  		<div className="measure center">
-				    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-				      <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-				      <div className="mt3">
-				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-				        <input
-				        	onChange={this.onEmailChange}
-				        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-				        	type="email" name="email-address" id="email-address"
-				        />
-				      </div>
-				      <div className="mv3">
-				        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-				        <input
-				        	onChange={this.onPasswordChange}
-					        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-					        type="password" name="password" id="password"
-				        />
-				      </div>
-				      {
-				      	this.state.isFailed ?
-					      	<pre className="alert alert-danger">
-							    	Wrong Email/Password
-							    </pre>
-								:
-									<pre></pre>
-				      }
-				    </fieldset>
-				    <div className="">
-				      <input
-				      	onClick={this.onSubmitSignIn}
-				      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-				      	type="submit"
-				      	value="Sign in"
-				      />
-				    </div>
-				  </div>
-				</main>
+  		<article className="mw6 mt4 center">
+	  		<div className="measure">
+			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+			      <legend className="f3 fw6 ph0 mh0">Sign In</legend>
+			      <div className="mt3">
+			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+			        <input
+			        	onChange={this.onEmailChange}
+			        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+			        	type="email" name="email-address" id="email-address"
+			        />
+			      </div>
+			      <div className="mv3">
+			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+			        <input
+			        	onChange={this.onPasswordChange}
+				        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+				        type="password" name="password" id="password"
+			        />
+			      </div>
+			      {
+			      	this.state.isFailed ?
+				      	<pre className="alert alert-danger">
+						    	Wrong Email/Password
+						    </pre>
+							:
+								<pre></pre>
+			      }
+			    </fieldset>
+			    <div className="">
+			      <input
+			      	onClick={this.onSubmitSignIn}
+			      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+			      	type="submit"
+			      	value="Sign in"
+			      />
+			    </div>
+			  </div>
 			</article>
   	);
   }
