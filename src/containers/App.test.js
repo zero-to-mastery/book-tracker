094d70d378eeaa08d54535dda6e88f4940b5ef8d
F1r3byte <f1r3byte@yandex.com>
2019-01-26 15:12:15 -1200

    attempt to get test to work n.4

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
