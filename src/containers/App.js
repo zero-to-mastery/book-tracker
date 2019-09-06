00a9daec434e0493b6e0048052b0d932573f2d4f
Aditya Shankar <49206592+aditya1906@users.noreply.github.com>
2019-09-06 07:13:48 +0530

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 
-import HomeNavbar from '../components/HomeNavbar/HomeNavbar';
 import TopNavbar from '../components/TopNavbar/TopNavbar';
 import AddPage from '../components/AddPage/AddPage';
 import LoginPage from '../components/LoginPage/LoginPage';
