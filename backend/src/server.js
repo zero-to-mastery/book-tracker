afd554eaa3f607609abe3de124ff01c4c8507e95
iphyokafor <sandraifeoma22@gmail.com>
2020-02-05 21:21:55 +0100

    created a database connection

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Mongodb connection string
+mongoose.connect(
+    process.env.DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => console.log('connected to db!'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port} yaay :)`);
+})
+
+export default app;
