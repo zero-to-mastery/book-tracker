35b8c2c7216ef0931e5422276625d4dc2a3a811e
VirtueAndTerror <50920185+VirtueAndTerror@users.noreply.github.com>
2020-04-14 16:46:35 -0400

    simpliefied routes with object destructuring

--- backend/src/controllers/usercontroller.js
+++ backend/src/controllers/usercontroller.js
@@ -7,60 +7,58 @@ import registerValidation from '../validation/register';
 import loginValidation from '../validation/login';
 
 export default {
-    register: async(req, res) => {
-        const { error } = registerValidation(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
-        const { email } = req.body;
-        const emailExist = await User.findOne({ email });
-        if (emailExist) {
-            return res.status(400).json({
-                message: 'Email already exists'
-            });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-
-
-        try {
-            const savedUser = await user.save();
-            res.send({
-                status: 201,
-                message: "User successfully registered",
-                user: user._id
-            });
-
-        } catch (err) {
-            res.status(400).json(err);
-        }
-    },
-
-
-    login: async(req, res) => {
-        const { error } = loginValidation(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
-
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) {
-            return res.status(400).send('Email doesn\'t exist');
-        }
-
-        const validPass = await bcrypt.compare(req.body.password, user.password);
-        if (!validPass) {
-            return res.status(400).send('Invalid Password');
-        }
-
-        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-        res.header('auth-token', token).send({
-            status: 200,
-            token,
-            message: 'logged in'
-        });
+  register: async (req, res) => {
+    const { email, password, name } = req.body;
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      return res.status(400).json({
+        message: 'Email already exists',
+      });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    try {
+      const savedUser = await user.save();
+      res.send({
+        status: 201,
+        message: 'User successfully registered',
+        user: user._id,
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send("Email doesn't exist");
+    }
+
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) {
+      return res.status(400).send('Invalid Password');
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send({
+      status: 200,
+      token,
+      message: 'logged in',
+    });
+  },
 };
